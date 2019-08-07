7a97cb3fc7138c65f7e537bdb080e4cebe6e89d7
Magnus Persson <magnus.persson@my.jcu.edu.au>
2019-08-07 10:31:22 +1000

    Initial commit for Prac 1

---
+++ src/DogTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class DogTest {
+    @Test
+    void testDefault() {
+        Dog dog = new Dog();
+        assertNull(dog.name);
+        assertEquals(0, dog.age);
+        assertNull(dog.breed);
+        assertNull(dog.shotStatus);
+    }
+}
